f4a04b0f98f112a358a4595de4facb8948c1c7e4
Mike Pappas <mpappas86@gmail.com>
2016-11-25 10:37:18 -0500

    Declare namespace

--- modelutils/__init__.py
+++ modelutils/__init__.py
@@ -1,3 +1,14 @@
+from __future__ import absolute_import
+
+# See http://peak.telecommunity.com/DevCenter/setuptools#namespace-packages
+try:
+    __import__('pkg_resources').declare_namespace(__name__)
+except ImportError:
+    from pkgutil import extend_path
+    __path__ = extend_path(__path__, __name__)
+
+
+
 def some(val):
     return val is not None
 
