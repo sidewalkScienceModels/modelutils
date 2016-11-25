7b7424be08dc54e38f961eb9eb6d7e3bea404756
Mike Pappas <mpappas86@gmail.com>
2016-11-25 10:34:24 -0500

    Make this an installable utility repo

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+requires = []
+with open(os.path.join(here, 'requirements.txt')) as f:
+    for line in f.read().splitlines():
+        if line.find('--') == -1:
+            requires.append(line)
+
+setup(
+    name='modelutils',
+    version='1.0.0',
+    description='Generic statistical and modelling utilities',
+    author='Mike Pappas',
+    install_requires=requires,
+    namespace_packages=['modelutils'],
+    packages=find_packages(include=['modelutils', 'modelutils.*']),
+    include_package_data=True
+)
